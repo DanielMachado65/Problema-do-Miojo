bbb618aca82d2befc52d55f3782c4ff20f36b43c
Daniel Machado <danielmachadopintos@gmail.com>
2020-01-28 16:40:17 -0300

    fix(variables): add variables

--- miojo.rb
+++ miojo.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: tru
 class Miojo
   def initialize(attrs)
     @time = attrs[:time].to_i
@@ -5,29 +6,27 @@ class Miojo
     @hourglass2 = attrs[:hourglass2].to_i
   end
 
-  def time_permitted?
+  def permitted?
     (@time > @hourglass1) || (@time > @hourglass2)
   end
 
   def calculate_minimum_time
-    if time_permitted?
-      return 'ampulheta não podem ter o tempo menor do que o tempo do prepararo do miojo'
-    end
+    return 'ampulheta não podem ter o tempo menor do que o tempo do prepararo do miojo' if permitted?
 
     min = [@hourglass1, @hourglass2].min
     max = [@hourglass1, @hourglass2].max
     min *= 2
 
-    valid = min - max
-    "#{valid==@time ? "Pode ser realizado o prepararo e o tempo minimo será: #{min} min" : 'Não é possivel fazer o miojo'}"
+    cooking_time = min - max
+    "#{cooking_time == @time ? "Pode ser realizado o prepararo e o tempo minimo será: #{min} min" : 'Não é possivel fazer o miojo'}"
   end
 end
 
 # running
 if ARGV.length == 3
-  p Miojo.new({ time: ARGV[0],
-             hourglass1: ARGV[1],
-             hourglass2: ARGV[2]}).calculate_minimum_time
+  p Miojo.new({time: ARGV[0],
+               hourglass1: ARGV[1],
+               hourglass2: ARGV[2]}).calculate_minimum_time
 else
-  p 'não foi inserido os parametros com sucesso'
+  p 'não foi inserido os parametros corretos'
 end
